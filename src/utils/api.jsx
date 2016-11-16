24078c1bdac1fbe38abc4a0508f3245446fdff4c
Oris John-Baptiste <orisjb@gmail.com>
2016-11-16 17:00:10 +0000

    initial commit

---
+++ src/utils/api.jsx
@@ -0,0 +1,16 @@
+var Fetch = require('whatwg-fetch');
+var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apiUrl = '&appid=65b93cf7a9ab730594800e8fa566b68f';
+
+module.exports = {
+    get: function(place) {
+        return fetch(rootUrl + place + apiUrl, {
+            headers: {
+                // No need for special headers
+            }
+        })
+        .then(function(response) {
+            return response.json();
+        });
+    }
+};
